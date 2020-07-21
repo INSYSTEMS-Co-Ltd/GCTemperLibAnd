16bf8e3380e619edb22ed73f0ce6330537cbe87b
mrsohn <mrsohn@okpos.co.kr>
2020-07-21 15:15:07 +0900

    샘플 앱

---
+++ GCTemperSampleApp/src/main/java/com/gchelathcare/heat/Main1QActivity.java
@@ -0,0 +1,203 @@
+package com.gchelathcare.heat;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+
+import com.greencross.gctemperlib.GCAlramType;
+import com.greencross.gctemperlib.GCFeverLib;
+import com.greencross.gctemperlib.IGCResult;
+import com.greencross.gctemperlib.common.CommonData;
+import com.greencross.gctemperlib.greencare.component.CDialog;
+import com.greencross.gctemperlib.greencare.util.PermissionUtil;
+import com.greencross.gctemperlib.greencare.util.cameraUtil.RuntimeUtil;
+import com.greencross.gctemperlib.util.PermissionUtils;
+
+public class Main1QActivity extends Activity {
+
+    private LinearLayout mProgress;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1q_main);
+
+        final GCFeverLib gcLib = new GCFeverLib(this);
+
+        mProgress = findViewById(R.id.progress_layout);
+
+        ((TextView) findViewById(R.id.lib_token_edittext)).setText(BuildConfig.GC_TOKEN);
+
+        // 녹십자 라이브러리 사용을 위한 토큰 등록
+        findViewById(R.id.gc_token_regist_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regsitGCToken();
+            }
+        });
+
+        // 녹십자 라이브러리 Push 서비스 등록
+        findViewById(R.id.push_token_regist_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regsitPushToken();
+            }
+        });
+
+        // 녹십자 체온관리 실행
+        findViewById(R.id.gc_excute_gc_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PermissionUtil.checkPermissions(Main1QActivity.this);
+                int permissionState = ActivityCompat.checkSelfPermission(Main1QActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
+                if (permissionState == PackageManager.PERMISSION_GRANTED) {
+                    gcLib.startGCMainActivity();
+                } else {
+                    ActivityCompat.requestPermissions(Main1QActivity.this, PermissionUtils.LOCATION_PERMS, CommonData.PERMISSION_REQUEST_GPS);
+                }
+            }
+        });
+
+        Switch switch1 = findViewById(R.id.alram_type_1);
+        Switch switch2 = findViewById(R.id.alram_type_2);
+        switch1.setChecked(gcLib.getSettingAlramService(GCAlramType.GC_ALRAM_TYPE_독려));
+        switch2.setChecked(gcLib.getSettingAlramService(GCAlramType.GC_ALRAM_TYPE_지역));
+        // 측정 독려 알림 수신 설정 변경
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                settingAlramService(GCAlramType.GC_ALRAM_TYPE_독려, checked);
+            }
+        });
+
+        // 지역 체온 알림 수신 설정 변경
+        switch2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                settingAlramService(GCAlramType.GC_ALRAM_TYPE_지역, checked);
+            }
+        });
+
+        findViewById(R.id.gc_lib_reset_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CDialog.showDlg(Main1QActivity.this, "초기화 하시겠습니까?")
+                        .setOkButton(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                gcLib.resetGCData();
+                            }
+                        })
+                        .setNoButton(getResources().getString(R.string.comm_cancel), null);
+
+            }
+        });
+    }
+
+    /**
+     * 최초 녹십자 라이브러리 사용을 위한 토큰 등록
+     */
+    private void regsitGCToken() {
+        final GCFeverLib gcLib = new GCFeverLib(this);
+        boolean isAvailable = gcLib.registGCToken(BuildConfig.GC_TOKEN);
+        String message = String.format("인증 %1$s 하였습니다.", isAvailable ? "성공" : "실패");
+        CDialog.showDlg(this, message);
+    }
+
+    /**
+     * 녹십자 라이브러리 Push 서비스 등록
+     */
+    public void regsitPushToken() {
+        final GCFeverLib gcLib = new GCFeverLib(this);
+        TextView tokenTv = findViewById(R.id.push_token_edittext);
+        TextView custNoTv = findViewById(R.id.cust_no_edittext);
+        String pushToken = tokenTv.getText().toString();
+        String customerNo = custNoTv.getText().toString();
+        String gender = null;
+
+        gcLib.registPushToken(pushToken, customerNo, gender, new IGCResult() {
+            @Override
+            public void onResult(boolean isSuccess, String message, Object data) {
+                if (isSuccess) {
+                    CDialog.showDlg(Main1QActivity.this, message);
+                } else {
+                    CDialog.showDlg(Main1QActivity.this, message);
+                }
+            }
+        });
+    }
+
+
+    /**
+     * 녹십자 라이브러리 Push 알람설정
+     * @param type
+     * @param isEnable
+     */
+    private void settingAlramService(GCAlramType type, boolean isEnable) {
+        final GCFeverLib gcLib = new GCFeverLib(this);
+
+        showProgress();
+        gcLib.settingAlramService(type, isEnable, new IGCResult() {
+            @Override
+            public void onResult(boolean isSuccess, String message, Object data) {
+                hideProgress();
+                if (isSuccess) {
+                    CDialog.showDlg(Main1QActivity.this, message);
+                } else {
+                    CDialog.showDlg(Main1QActivity.this, message);
+                }
+            }
+        });
+    }
+
+
+    /**
+     * 위치권한 설정 후
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CommonData.PERMISSION_REQUEST_GPS) {
+            if (RuntimeUtil.verifyPermissions(Main1QActivity.this, grantResults)) {
+                final GCFeverLib gcLib = new GCFeverLib(this);
+                if (gcLib.isAvailableGCToken()) {
+                    gcLib.startGCMainActivity();
+                } else {
+                    CDialog.showDlg(Main1QActivity.this, "인증 후 이용 가능합니다.");
+                }
+            }
+        }
+    }
+
+
+    private void showProgress() {
+        mProgress.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgress() {
+        mProgress.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mProgress.getVisibility() == View.VISIBLE)
+            hideProgress();
+        else
+            super.onBackPressed();
+    }
+
+}

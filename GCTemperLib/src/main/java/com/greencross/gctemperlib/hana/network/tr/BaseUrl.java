a1d00b7b893d4fb36d7a52067be073189d1485c1
최진영 <wlsdud8598@gmail.com>
2020-08-26 14:05:05 +0900

    API 주소 테스트, 운영 추가

--- GCTemperLib/src/main/java/com/greencross/gctemperlib/hana/network/tr/BaseUrl.java
+++ GCTemperLib/src/main/java/com/greencross/gctemperlib/hana/network/tr/BaseUrl.java
@@ -8,6 +8,8 @@ public class BaseUrl {
 	public static String COMMON_URL;
 
 	static {
+		// https://api.devgc.com/hana 테스트 주소
+		// https://wkd.walkie.co.kr/hana 운영서버 주소
 		if(RELEASE_MODE){
             COMMON_URL = "https://api.devgc.com/hana";
 
